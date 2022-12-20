5f05d6dbb897852bdf615406c731663cde00b246
MrJohnB <jmbianchi@gmail.com>
2022-12-20 13:13:59 -0800

    Added ValueObject abstract base class (for non entities) and DateRange (as a value object) to the Shared project.

---
+++ src/LiteBulb.OatShop.Shared/ValueObjects/DateRange.cs
@@ -0,0 +1,18 @@
+﻿namespace LiteBulb.OatShop.Shared.ValueObjects;
+public class DateRange : ValueObject
+{
+    public DateTime StartDate { get; }
+    public DateTime EndDate { get; }
+
+    public DateRange(DateTime startDate, DateTime endDate)
+    {
+        StartDate = startDate;
+        EndDate = endDate;
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return StartDate;
+        yield return EndDate;
+    }
+}
